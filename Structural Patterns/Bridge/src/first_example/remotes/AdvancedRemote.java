e1fd75660aee2fa0df25f4ba39979a4782defaf0
baldzhiyski__ <hristo_baldjiiski04@abv.bg>
2024-02-28 23:22:47 +0200

    Adding a bridge example

---
+++ Structural Patterns/Bridge/src/first_example/remotes/AdvancedRemote.java
@@ -0,0 +1,15 @@
+package first_example.remotes;
+
+import first_example.devices.Device;
+
+public class AdvancedRemote extends   BasicRemote{
+    public AdvancedRemote(Device device) {
+        super.device = device;
+    }
+
+    public void mute() {
+        System.out.println("Remote: mute");
+        device.setVolume(0);
+    }
+    
+}
---
+++ Structural Patterns/Bridge/src/first_example/remotes/Remote.java
@@ -0,0 +1,13 @@
+package first_example.remotes;
+
+public interface Remote {
+    void power();
+
+    void volumeDown();
+
+    void volumeUp();
+
+    void channelDown();
+
+    void channelUp();
+}
